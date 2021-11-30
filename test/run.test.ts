5523e5acd382f4ef7a5dca4bb819e3bfddf2ec34
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-01 03:52:24 +0800

    chore: enable tests, close #5

---
+++ test/run.test.ts
@@ -0,0 +1,43 @@
+import { resolve } from 'path'
+import { loadConfig, sourcePackageJsonFields } from '../src'
+
+it('load', async() => {
+  const result = await loadConfig({
+    sources: [
+      {
+        files: 'un.config',
+      },
+      sourcePackageJsonFields({
+        fields: 'un',
+      }),
+      {
+        files: 'rewrite.js',
+        extensions: [],
+        transform: (source: string) => {
+          const moduleName = 'stub'
+          const prefix = `
+  let __unconfig_data;
+  let __unconfig_stub = function (data) { __unconfig_data = data };
+  __unconfig_stub.default = (data) => { __unconfig_data = data };
+  `
+          const suffix = 'export default __unconfig_data;'
+          let code = source
+            .replace(new RegExp(`import (.+?) from (['"])${moduleName}\\2`), 'const $1 = __unconfig_stub;')
+            .replace('export default', 'const __unconfig_default = ')
+          if (code.includes('__unconfig_default'))
+            code += '\nif (typeof __unconfig_default === "function") __unconfig_default();'
+          return `${prefix}${code}${suffix}`
+        },
+      },
+    ],
+    cwd: resolve(__dirname, 'fixtures'),
+    defaults: {
+      defaults: 'default',
+      deep: { foo: 'hi' },
+    },
+    merge: true,
+  })
+
+  expect(result.config).toMatchSnapshot()
+  expect(result.sources.length).toMatchSnapshot('files')
+})
