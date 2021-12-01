8ce524f3c4fb2b92d1171815b9b1d2566404abf9
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-01 22:58:09 +0800

    refactor: allow creating loader to reuse

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { findUp } from './fs'
 
 export * from './types'
 
-export async function loadConfig<T>(options: LoadConfigOptions): Promise<LoadConfigResult<T>> {
+export function createConfigLoader(options: LoadConfigOptions) {
   const sources = toArray(options.sources || [])
   const {
     cwd = process.cwd(),
@@ -17,54 +17,78 @@ export async function loadConfig<T>(options: LoadConfigOptions): Promise<LoadCon
   } = options
 
   const results: LoadConfigResult<any>[] = []
+  let matchedFiles: [LoadConfigSource, string[]][] | undefined
 
-  for (const source of sources) {
-    const { extensions = defaultExtensions } = source
+  async function findConfigs() {
+    if (matchedFiles == null)
+      matchedFiles = []
 
-    const flatTargets = toArray(source?.files || [])
-      .flatMap(file => !extensions.length
-        ? [file]
-        : extensions.map(i => i ? `${file}.${i}` : file),
-      )
+    matchedFiles.length = 0
+    for (const source of sources) {
+      const { extensions = defaultExtensions } = source
 
-    if (!flatTargets.length)
-      continue
+      const flatTargets = toArray(source?.files || [])
+        .flatMap(file => !extensions.length
+          ? [file]
+          : extensions.map(i => i ? `${file}.${i}` : file),
+        )
 
-    const files = await findUp(flatTargets, { cwd, stopAt: options.stopAt, multiple: merge })
+      const files = await findUp(flatTargets, { cwd, stopAt: options.stopAt, multiple: merge })
 
-    if (!files.length)
-      continue
-
-    if (!merge) {
-      const result = await loadConfigFile(files[0], source)
-      if (result) {
-        return {
-          config: defu(result.config, defaults),
-          sources: result.sources,
-        }
-      }
+      matchedFiles.push([source, files])
     }
-    else {
-      results.push(
-        ...(await Promise.all(
-          files.map(file => loadConfigFile(file, source)))
-        ).filter(notNullish),
-      )
-    }
+
+    return matchedFiles.flatMap(i => i[1])
   }
 
-  if (!results.length) {
+  async function load(force = false) {
+    if (matchedFiles == null || force)
+      await findConfigs()
+
+    for (const [source, files] of matchedFiles!) {
+      if (!files.length)
+        continue
+
+      if (!merge) {
+        const result = await loadConfigFile(files[0], source)
+        if (result) {
+          return {
+            config: defu(result.config, defaults),
+            sources: result.sources,
+          }
+        }
+      }
+      else {
+        results.push(
+          ...(await Promise.all(
+            files.map(file => loadConfigFile(file, source)))
+          ).filter(notNullish),
+        )
+      }
+    }
+
+    if (!results.length) {
+      return {
+        config: defaults,
+        sources: [],
+      }
+    }
+
     return {
-      config: defaults,
-      sources: [],
+      // @ts-expect-error
+      config: defu(...results.map(i => i.config), defaults),
+      sources: results.map(i => i.sources).flat(),
     }
   }
 
   return {
-    // @ts-expect-error
-    config: defu(...results.map(i => i.config), defaults),
-    sources: results.map(i => i.sources).flat(),
+    load,
+    findConfigs,
   }
+}
+
+export async function loadConfig<T>(options: LoadConfigOptions): Promise<LoadConfigResult<T>> {
+  return createConfigLoader(options).load()
 }
 
 async function loadConfigFile<T>(filepath: string, source: LoadConfigSource<T>): Promise<LoadConfigResult<T> | undefined> {
